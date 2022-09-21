cddbfa66634bdfeaa6190f7b2aa0368fa01c9e5f
Madanjaryal <45655921+Madanjaryal@users.noreply.github.com>
2022-09-21 22:58:44 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+import LayoutFlow from './WebFlow';
+
+export default function App() {
+  return (
+    <div>
+      <LayoutFlow />
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-endky1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-endky1)
